f9920960ccc84e6da7ed998ef1cd368def348293
dymayday <dymayday@gmail.com>
2018-12-01 16:49:24 +0100

    :wrench: Add entry point install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Setting up the package managers
+UNAME=`uname -r`
+if [[ "$UNAME" == *"ARCH" ]]; then
+    # Let's update the packages sources
+    sudo pacman -Syyu
+    # sudo pacman -S yaourt
+    PACMAN="yaourt -S"
+else
+    # Let's update the packages sources
+    sudo apt update
+    PACMAN="sudo apt install"
+fi
+
+# Let's install the programs now
+PACKAGES="vim neovim htop git"
+echo "Installing ${PACKAGES}..."
+$PACMAN $PACKAGES
+
+# Let's loop on every section's directory and run their install script
+declare -a section_array=("terminal" "tmux" "rust")
+for section in "${section_array[@]}"; do
+    echo "Setting up '${section}'..."
+    # bash ./$section/install.sh
+done

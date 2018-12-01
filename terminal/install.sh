d4be3debc9ddc9790e406c86b83f36718e1f3fa2
dymayday <dymayday@gmail.com>
2018-12-01 16:57:55 +0100

    :construction: WIP terminal setup

--- terminal/install.sh
+++ terminal/install.sh
@@ -4,7 +4,9 @@ declare -a shell_array=("bash" "zsh")
 for shell_name in "${shell_array[@]}"; do
     echo "Setting up '${shell_name}'..."
     # bash ${shell_name}/
+    echo "Copying ./${shell_name}/${shell_name}rc to $HOME/.${shell_name}rc"
     cp ./${shell_name}/${shell_name}rc $HOME/.${shell_name}rc
+    echo ""
 done
 
 # Next, we focus on the common parts

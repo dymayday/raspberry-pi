2fe2a31a9c83ab2e4e98a64bce67428837d2280e
dymayday <dymayday@gmail.com>
2018-12-01 17:02:17 +0100

    :construction: WIP tmux setup

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,4 +3,4 @@
 TPM_DIR="$HOME/.tmux/plugins/tpm"
 if [[ -s $TPM_DIR ]]; then rm -rf $TPM_DIR ; fi
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-cp ./tmux.conf $HOME/.tmux.conf
+cp tmux.conf $HOME/.tmux.conf

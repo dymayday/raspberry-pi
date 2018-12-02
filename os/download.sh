ced6fb85472930e5c2cec64ad788c7b739bdc7de
dymayday <dymayday@gmail.com>
2018-12-02 14:26:27 +0100

    :wrench: Relative directory for archives downloading

--- os/download.sh
+++ os/download.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+ARCHIVES_DIR="${DIR}/archives"
+
 # Archlinux section
 OSNAME="ArchLinuxARM-rpi-3-latest.tar.gz"
-curl -L http://os.archlinuxarm.org/os/${OSNAME} --output archives/${OSNAME}
+curl -L http://os.archlinuxarm.org/os/${OSNAME} --output ${ARCHIVES_DIR}/${OSNAME}
 
+# For the time being, we will only focus on the 'Archlinux' OS
 exit
 
 # Rasbian section
 OSNAME="raspbian_lite_latest"
-curl -Lk https://downloads.raspberrypi.org/${OSNAME} --output archives/${OSNAME}.zip
+curl -Lk https://downloads.raspberrypi.org/${OSNAME} --output ${ARCHIVES_DIR}/${OSNAME}.zip

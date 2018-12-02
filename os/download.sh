4400d2db83e2cd3c4830201c501447bb6f5927df
dymayday <dymayday@gmail.com>
2018-12-02 14:11:19 +0100

    :wrench: Move OS download directory to 'archives'

--- os/download.sh
+++ os/download.sh
@@ -2,10 +2,10 @@
 
 # Archlinux section
 OSNAME="ArchLinuxARM-rpi-3-latest.tar.gz"
-curl -L http://os.archlinuxarm.org/os/${OSNAME} --output ${OSNAME}
+curl -L http://os.archlinuxarm.org/os/${OSNAME} --output archives/${OSNAME}
 
 exit
 
 # Rasbian section
 OSNAME="raspbian_lite_latest"
-curl -Lk https://downloads.raspberrypi.org/${OSNAME} --output ${OSNAME}.zip
+curl -Lk https://downloads.raspberrypi.org/${OSNAME} --output archives/${OSNAME}.zip

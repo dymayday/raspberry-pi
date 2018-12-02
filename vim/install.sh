6abd65ef049d3641ed1c7072fff3bf2c659c35a7
dymayday <dymayday@gmail.com>
2018-12-02 16:53:51 +0100

    :bug: Fix Vundle target directory

--- vim/install.sh
+++ vim/install.sh
@@ -13,10 +13,11 @@ cp ${DIR}/vimrc $HOME/.vimrc
 [ ! -s $HOME/.vim.backup ] && mkdir $HOME/.vim.backup
 
 # Let's install 'Vundle', a plugins helper for Vim
-if [[ ! -s $HOME/.vim/bundle/Vundle.vim ]]; then
-    mkdir -p /opt/usr/.vim/bundle && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+BUNDLE_DIR="$HOME/.vim/bundle"
+if [[ ! -s $BUNDLE_DIR/Vundle.vim ]]; then
+    mkdir -p $BUNDLE_DIR && git clone https://github.com/VundleVim/Vundle.vim.git ~/$BUNDLE_DIR/Vundle.vim
     vim +PluginInstall +qall
-    git clone --depth 1 https://github.com/junegunn/fzf.git /opt/usr/.fzf && /opt/usr/.fzf/install
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install
 fi
 
 # And then, let's install the plugins from the configuration file: 'vimrc'

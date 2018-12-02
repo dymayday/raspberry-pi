2351b97d7d4d40a3310c02a949725747b544d4db
dymayday <dymayday@gmail.com>
2018-12-02 15:34:40 +0100

    :wrench: Add relative path handling to tmux install script

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Defining the directory of the plugin manager
 TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+# Get the directory path of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 if [[ -s $TPM_DIR ]]; then rm -rf $TPM_DIR ; fi
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-cp tmux.conf $HOME/.tmux.conf
+cp ${DIR}/tmux.conf $HOME/.tmux.conf

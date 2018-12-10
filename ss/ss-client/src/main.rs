571436577e281a34c5d266170c2f2c53a5f78f2d
dymayday <dymayday@gmail.com>
2018-12-10 14:52:51 +0100

    :construction: Playing around with datetime

--- ss/ss-client/src/main.rs
+++ ss/ss-client/src/main.rs
@@ -1,3 +1,17 @@
+extern crate chrono;
+
+mod sensors;
+// use sensors::Sensor;
+use chrono::SubsecRound;
+
 fn main() {
-    println!("Hello, world!");
+    println!();
+
+    use chrono::{DateTime, Utc};
+    let ts = sensors::Index::<DateTime<Utc>>::new();
+
+    println!("dt = {:?}", ts);
+    for i in 0..9 {
+        println!("dtr({}) = {:?}", i, ts.value[0].round_subsecs(i));
+    }
 }

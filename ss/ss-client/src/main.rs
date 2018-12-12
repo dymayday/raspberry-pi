66ea92dcd2c8c754656dd0115a48b70e75bbcacb
dymayday <dymayday@gmail.com>
2018-12-12 16:10:47 +0100

    :construction: Trying out wifi scanner library

--- ss/ss-client/src/main.rs
+++ ss/ss-client/src/main.rs
@@ -1,17 +1,19 @@
 extern crate chrono;
 
-mod sensors;
-// use sensors::Sensor;
-use chrono::SubsecRound;
+mod data;
 
 fn main() {
     println!();
 
-    use chrono::{DateTime, Utc};
-    let ts = sensors::Index::<DateTime<Utc>>::new();
+    // use chrono::{SubsecRound, Utc};
+    // let ts = Utc::now();
+    //
+    // println!("dt = {:?}", ts);
+    // for i in 0..9 {
+    //     println!("dtr({}) = {:?}", i, ts.round_subsecs(i));
+    // }
 
-    println!("dt = {:?}", ts);
-    for i in 0..9 {
-        println!("dtr({}) = {:?}", i, ts.value[0].round_subsecs(i));
-    }
+    use wifiscanner;
+    let wscan = wifiscanner::scan();
+    println!("Wifi scanner: {:#?}", wscan);
 }
